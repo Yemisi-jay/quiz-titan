0ff39d70af06682591cd5cf8d1bbf96ea214c5c0
Oluwayemisi <anuoluwayemisi@gmail.com>
2024-09-20 18:08:52 +0100

    updates

--- trivia_project/settings.py
+++ trivia_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-&n&5h_*c$0v9*%gd%p&&#qm#6$cqu#9__f+@f-&l37*1$dgtcx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
 
 # Application definition
